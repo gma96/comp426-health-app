ce40696176c0bcaa9dcbf16bdec73736396c0794
pmr1027 <pmr1027@live.unc.edu>
2017-12-02 15:32:01 -0500

    logging, cleanup

--- backend/mock/dist/server.js
+++ backend/mock/dist/server.js
@@ -1,17 +1,20 @@
+'use strict';
+
 // Express Stuff
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const PORT = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var PORT = process.env.PORT || 8080;
 // Logger
-const morgan = require('morgan');
+var morgan = require('morgan');
+var log = require('../../../libs/logger');
 
 // Mongoose
 // const { mongoose } = require('./api/config/Mongoose');
 // const User = mongoose.model('Users');
 
-// // Controllers 
+// // Controllers
 // const UserController =  require('./api/controllers/UserController');
 
 // App Configuration
@@ -21,12 +24,12 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
 // The Routers
-const routers = (() => {
-	return {
-		authed: express.Router(),
-		public: express.Router()
-	};
-})();
+var routers = function () {
+  return {
+    authed: express.Router(),
+    public: express.Router()
+  };
+}();
 
 // Install Routers
 app.use(routers.public);
@@ -35,23 +38,23 @@ app.use(routers.public);
 // Routes
 // require('./api/routes/UserRoutes')(routers);
 
-routers.public.get('/hello', (req, res) => {
-	let response = {};
+routers.public.get('/hello', function (req, res) {
+  var response = {};
+  response.token_decoded = req.decoded;
 
-	response.token_decoded = req.decoded;
+  var db = {
+    '59fa3bcd17db4d042dfab825': [{ some: 'hi' }, { some: 'jkldsjafja' }, { some: 'yo dog' }, { i_love: 'leah dog' }],
+    '5a0e66b1b60fa7341c357487': [{ i_love: 'whoever' }, { some: 'data' }, { some: 'data' }, { some: 'data' }]
+  };
 
-	let db = {
-		'59fa3bcd17db4d042dfab825': [{ some: 'hi preston' }, { some: 'jkldsjafja' }, { some: 'yo dog' }, { i_love: 'leah dog' }],
-		'5a0e66b1b60fa7341c357487': [{ i_love: 'preston' }, { some: 'data' }, { some: 'data' }, { some: 'data' }]
-
-		//response.data = db[req.decoded._id];
-	};res.json(db);
+  // response.data = db[req.decoded._id];
+  res.json(db);
 });
 
-routers.authed.all('*', (req, res) => {
-	res.status(404).json({ message: '404' });
+routers.authed.all('*', function (req, res) {
+  res.status(404).json({ message: '404' });
 });
 
 // Start Application
 module.exports = app.listen(PORT);
-console.log(`Mock Server Running on Port: ${PORT}`);
+log.info('Mock Server Running on Port: ' + PORT);
--- backend/mock/src/server.js
+++ backend/mock/src/server.js
@@ -6,26 +6,27 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 // Logger
 const morgan = require('morgan');
+const log = require('../../../libs/logger');
 
 // Mongoose
 // const { mongoose } = require('./api/config/Mongoose');
 // const User = mongoose.model('Users');
 
-// // Controllers 
+// // Controllers
 // const UserController =  require('./api/controllers/UserController');
 
 // App Configuration
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
 // The Routers
 const routers = (() => {
-	return {
-		authed: express.Router(),
-		public: express.Router(),
-	};
+  return {
+    authed: express.Router(),
+    public: express.Router(),
+  };
 })();
 
 // Install Routers
@@ -36,34 +37,32 @@ app.use(routers.public);
 // require('./api/routes/UserRoutes')(routers);
 
 routers.public.get('/hello', (req, res) => {
-	let response = {};
+  let response = {};
+  response.token_decoded = req.decoded;
 
-	response.token_decoded = req.decoded;
+  let db = {
+    '59fa3bcd17db4d042dfab825': [
+      {some: 'hi'},
+      {some: 'jkldsjafja'},
+      {some: 'yo dog'},
+      {i_love: 'leah dog'},
+    ],
+    '5a0e66b1b60fa7341c357487': [
+    {i_love: 'whoever'},
+    {some: 'data'},
+    {some: 'data'},
+    {some: 'data'},
+    ],
+  };
 
-	let db = {
-		'59fa3bcd17db4d042dfab825': [
-			{some: 'hi preston'},
-			{some: 'jkldsjafja'},
-			{some: 'yo dog'},
-			{i_love: 'leah dog'},
-		],
-		'5a0e66b1b60fa7341c357487': [
-			{i_love: 'preston'},
-			{some: 'data'},
-			{some: 'data'},
-			{some: 'data'},
-		],
-	}
-
-	//response.data = db[req.decoded._id];
-	res.json(db);
-
+  // response.data = db[req.decoded._id];
+  res.json(db);
 });
 
-routers.authed.all('*',  (req, res) => {
-	res.status(404).json({message: '404'});
+routers.authed.all('*', (req, res) => {
+  res.status(404).json({message: '404'});
 });
 
 // Start Application
 module.exports = app.listen(PORT);
-console.log(`Mock Server Running on Port: ${PORT}`);
+log.info(`Mock Server Running on Port: ${PORT}`);
