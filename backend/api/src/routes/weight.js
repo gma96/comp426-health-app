77533d30114edee29eae70d4be3d9d3e051f2d3e
pmr1027 <pmr1027@live.unc.edu>
2017-12-10 02:03:20 -0500

    generics and typos

---
+++ backend/api/src/routes/weight.js
@@ -0,0 +1,15 @@
+// @flow
+'use strict';
+const _name:string = 'weight';
+const controller:Object = require(`../controllers/${_name}`); // :( Flow no like
+module.exports = (router: Object) => {
+  router.route(`${_name}`)
+    .post(controller.create);
+  // Need to think about: should this be part of user path or own path???
+  // /users/me/${name}/:_id?
+  // we use me in the path because token provides user identity
+  router.route(`${_name}/:_id`)
+    .get(controller.read)
+    .patch(controller.update)
+    .delete(controller.delete);
+};
