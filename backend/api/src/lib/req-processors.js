f38e60d6bfae60baf76e602828b0cde12db5bd95
pmr1027 <pmr1027@live.unc.edu>
2017-12-11 03:34:56 -0500

    RAML Specs, related updates

---
+++ backend/api/src/lib/req-processors.js
@@ -0,0 +1,29 @@
+// @flow
+'use strict';
+// NPM Modules
+const _difference = require('lodash/difference');
+
+const processors:Object = {};
+
+// Field Processor
+processors.fields = (_path:string, _fields:Array<string>, reqFields:string) => {
+  return new Promise((resolve, reject) => {
+    let fields:?Array<string> = reqFields.toLowerCase().split(',') || null;
+    let e:Object = {
+      type: 'ResourceFieldsError',
+      dataPath: _path,
+      message: 'No field values provided, but field parameter present',
+    };
+    if (fields) {
+      let difference = _difference(fields, _fields);
+      if (difference.length > 0) {
+        e.message = 'Invalid field values';
+        e.values = difference;
+        return reject(e);
+      }
+      return resolve();
+    } else return reject(e);
+  });
+};
+
+module.exports = processors;
